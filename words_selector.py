81dbb7747a513d93438432b7b838d7489c88946e
sinistro25 <wagner.brasileiro2011@hotmail.com>
2020-05-24 12:47:51 -0300

    Add text for function call in the code typing session.

--- words_selector.py
+++ words_selector.py
@@ -48,6 +48,19 @@ def int_comparison_generator(size=20):
     return tmp
 
 
+def function_call_generator(size=20):
+    tmp = []
+    for _ in range(size):
+        fcall = random.choice(words) + "("
+        nargs = random.randint(0, 2)
+        for i in range(nargs-1):
+            fcall += random.choice(words) + ","
+        fcall += random.choice(words)
+        fcall += ")"
+        tmp.append(fcall)
+    return tmp
+
+
 def assignment_generator(values, size=40):
     tmp = []
     for _ in range(size):
@@ -70,11 +83,23 @@ def code_generator(size=10):
     tmp += assignment_generator(array_generator([i for i in range(size)], size=size) +
                                 enclose(int_comparison_generator(size=size), "(", ")"))
     tmp += int_comparison_generator(size=size)
+    tmp += function_call_generator(size=size)
+    tmp = partially_add_semicolon(tmp)
     random.shuffle(tmp)
     return tmp[:size]
 
 
+def partially_add_semicolon(text, p=0.2):
+    tmp = []
+    for word in text:
+        if random.random() < p:
+            tmp.append(word + ";")
+        else:
+            tmp.append(word)
+    return tmp
+
+
 if __name__ == "__main__":
-    code = code_generator(size=10)
-    print(code)
-    print(len(code))
+    fgen = function_call_generator(10)
+    fgen = partially_add_semicolon(fgen)
+    print(fgen)
